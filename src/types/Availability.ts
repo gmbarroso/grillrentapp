13c9e912bfb352a46fa2b7119d7e81995d838893
gmbarroso <barroso.guilherme@gmail.com>
2026-03-20 14:19:59 -0300

    refactor: remove unused availability and organization types, and JWT utility functions

--- src/types/Availability.ts
+++
@@ -1,7 +0,0 @@
-export interface Availability {
-    id: string
-    resourceType: "daily" | "hourly"
-    resourceId: string
-  }
-  
-  
--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Booking"
-export * from "./Availability"
-export * from "./Resource"
-export * from "./User"
-export * from "./Message"
-export * from "./Organization"

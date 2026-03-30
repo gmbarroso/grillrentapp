ac1dc3eb0a06ad97d56e47d7645d69b03b0b9195
gmbarroso <barroso.guilherme@gmail.com>
2026-03-29 21:49:44 -0300

    feat: add redirect for specific hostname to enhance routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import "./index.css"
 import "./i18n"
 import { configureConsoleLevel } from "./utils/console"
 
+if (typeof window !== "undefined" && window.location.hostname === "chacarasacopa.vercel.app") {
+  const target = `https://seuze.tech${window.location.pathname}${window.location.search}${window.location.hash}`
+  window.location.replace(target)
+}
+
 configureConsoleLevel()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
